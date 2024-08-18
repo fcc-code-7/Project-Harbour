ad317c7cb9f280892bbcbee7fdb38e0442ea5ba1
FARAZ <02-134211-058@student.bahria.edu.pk>
2024-08-18 11:22:56 +0500

    Add project files.

---
+++ FYP.Web/Controllers/ProjectController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FYP.Web.Controllers
+{
+    public class ProjectController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Projects()
+        {
+            return View();
+        }
+    }
+}
---
+++ FYP.Web/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FYP.Web.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Profile()
+        {
+            return View();
+        }
+        public IActionResult Edit()
+        {
+            return View();
+        }
+        public IActionResult Message()
+        {
+            return View();
+        }
+    }
+}
